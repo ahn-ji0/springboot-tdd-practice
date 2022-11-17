b99fa3e6b3e08d6c8293af2aaa576837e432675c
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-17 16:18:09 +0900

    Add: AllArgsConstructor

--- src/main/java/com/springboot/springboottddpractice/domain/dto/UserAddResponse.java
+++ src/main/java/com/springboot/springboottddpractice/domain/dto/UserAddResponse.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class UserAddResponse {
     private String username;
     private String message;
--- src/main/java/com/springboot/springboottddpractice/domain/dto/UserGetResponse.java
+++ src/main/java/com/springboot/springboottddpractice/domain/dto/UserGetResponse.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class UserGetResponse {
     private Long id;
     private String username;
