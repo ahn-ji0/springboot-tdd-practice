e7561d2b82eaf95ee731dbf5dd545059b7d54891
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-17 13:40:25 +0900

    User Request Dto

---
+++ src/main/java/com/springboot/springboottddpractice/domain/dto/UserRequest.java
@@ -0,0 +1,20 @@
+package com.springboot.springboottddpractice.domain.dto;
+
+import com.springboot.springboottddpractice.domain.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserRequest {
+    String username;
+    String password;
+
+    public User toEntity(){
+        return new User(this.username,this.password);
+    }
+}
