8ea32d5e55b5583522cf9efdf4e1ca8caeaeaab1
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-17 13:39:25 +0900

    init commit

---
+++ src/main/java/com/springboot/springboottddpractice/SpringbootTddPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springboottddpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTddPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTddPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/springboottddpractice/SpringbootTddPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springboottddpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTddPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
