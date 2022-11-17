75453be37fc13cebef932d385619fbbcdc24195e
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-17 13:39:54 +0900

    User Repository

---
+++ src/main/java/com/springboot/springboottddpractice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.springboot.springboottddpractice.repository;
+
+import com.springboot.springboottddpractice.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findAllByUsername(String username);
+}
